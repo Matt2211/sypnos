da13bc64f98633bc0017d72995c61186dc0f828c
Matt <matteofelicidesign@gmail.com>
2024-01-30 00:57:23 +0000

    test

---
+++ components/CardTest.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card font-bold">
+        hello
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+   <cardTest />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
